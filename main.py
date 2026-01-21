8c8378945cc30e645fecbdae2772db7dce010ed1
Abdullah Yusuf Yıldırım <tt.shirted@gmail.com>
2026-01-21 17:56:56 +0300

    bug fixexd

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
 from pdf2docx import parse
 
-pdf_file ="/Users/taha/Desktop/pdf_file.pdf"
-docx_file ="/Users/taha/Desktop/docx_file.docx"
+pdf_file ="/Users/example_user/Desktop/pdf_file.pdf"
+docx_file ="/Users/example_user/Desktop/docx_file.docx"
 
 
 parse(pdf_file, docx_file)
