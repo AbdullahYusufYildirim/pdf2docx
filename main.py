bed02a59fcf26f706f7bba0165804a73edf1f153
Abdullah Yusuf Yıldırım <tt.shirted@gmail.com>
2026-01-21 17:19:18 +0300

    path error  fixed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
 from pdf2docx import parse
 
-pdf_file ="/Users/taha/Desktop"
-docx_file ="/Users/taha/Desktop"
+pdf_file ="/Users/taha/Desktop/pdf_file.pdf"
+docx_file ="/Users/taha/Desktop/docx_file.docx"
 
 
 parse(pdf_file, docx_file)
